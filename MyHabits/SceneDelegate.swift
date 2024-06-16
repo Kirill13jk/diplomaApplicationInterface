b22867d3ab68d511c269a44760db3fe5a1ae07b0
Kirill <onlyjesus13@icloud.com>
2024-06-16 15:28:54 +0500

    second commit

--- MyHabits/SceneDelegate.swift
+++ MyHabits/SceneDelegate.swift
@@ -11,23 +11,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       
         let habitsViewController = HabitsViewController()
         habitsViewController.title = "Привычки"
+        habitsViewController.view.backgroundColor = .systemBackground
         
         let infoViewController = InfoViewController()
         infoViewController.title = "Информация"
+        infoViewController.view.backgroundColor = .systemBackground
         
         let tabBarController = UITabBarController()
-        tabBarController.tabBar.barTintColor = .systemGray5
-        tabBarController.tabBar.tintColor = .purple
         
-        habitsViewController.tabBarItem = UITabBarItem(title: "Привычки", image: UIImage(systemName: "list.bullet"), tag: 0)
-        infoViewController.tabBarItem = UITabBarItem(title: "Информация", image: UIImage(systemName: "info.circle"), tag: 1)
+        habitsViewController.tabBarItem = UITabBarItem(title: "Привычки", image: UIImage(named: "habits"), tag: 0)
+        infoViewController.tabBarItem = UITabBarItem(title: "Информация", image: UIImage(named: "info"), tag: 1)
         
         let controllers = [habitsViewController, infoViewController]
         tabBarController.viewControllers = controllers.map {
             UINavigationController(rootViewController: $0)
         }
         
-        tabBarController.selectedIndex = 0
+        tabBarController.selectedIndex = 1
         
         window.rootViewController = tabBarController
         window.makeKeyAndVisible()
@@ -35,13 +35,34 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window = window
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) { }
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
 
-    func sceneDidBecomeActive(_ scene: UIScene) { }
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
 
-    func sceneWillResignActive(_ scene: UIScene) { }
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
 
-    func sceneWillEnterForeground(_ scene: UIScene) { }
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
 
-    func sceneDidEnterBackground(_ scene: UIScene) { }
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+
+
 }
+
